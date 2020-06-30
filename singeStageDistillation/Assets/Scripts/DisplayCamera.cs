493104d8381eabce477ca2b53b10d349c3883643
@MinhDungDo <minh.do@st.ovgu.de>
2020-06-30 02:10:56 +0200

    Merge environment to main branch

---
+++ singeStageDistillation/Assets/Scripts/DisplayCamera.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayCamera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject firstPersonCamera;
+    public GameObject mainCamera;
+
+    AudioListener firstPersonAudioLis;
+    AudioListener mainCameraAudioLis;
+
+    void Start()
+    {
+        //Get Camera Listeners
+        firstPersonAudioLis = firstPersonCamera.GetComponent<AudioListener>();
+        mainCameraAudioLis = mainCamera.GetComponent<AudioListener>();
+
+    }
+
+    public void displayCamera()
+    {
+        if(firstPersonCamera != null && mainCamera != null)
+        {
+            //Get Camera Listeners
+            firstPersonAudioLis = firstPersonCamera.GetComponent<AudioListener>();
+            mainCameraAudioLis = mainCamera.GetComponent<AudioListener>();
+
+            bool isActiveFP;
+            isActiveFP = firstPersonCamera.activeSelf;
+            bool isActiveMain;
+            isActiveMain = mainCamera.activeSelf;
+            print(isActiveFP);
+            print(isActiveMain);
+            //Set FPS camera
+            firstPersonCamera.SetActive(!isActiveFP);
+            firstPersonAudioLis.enabled = !isActiveFP;
+            //Set main camera
+            mainCamera.SetActive(!isActiveMain);
+            mainCameraAudioLis.enabled = !isActiveMain;
+        }
+    }
+}
